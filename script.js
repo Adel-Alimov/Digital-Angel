cd3613628a485addbfc12175b0f2dd9b90ff91a1
Adel-Alimov <adel.alimov@list.ru>
2025-12-08 00:06:42 +0400

    change install and add form

--- script.js
+++ script.js
@@ -5,20 +5,31 @@ const btnTg = document.getElementById('button_tg');
 const installLeft = document.getElementById('install_left')
 const installRight = document.getElementById('install_right')
 
+const inactiveColor = '#e5e4df'; 
+const activeColor = '#f0f0f0';
 btnTg.onclick = function(){
-    installRight.style.backgroundColor = 'white';
-    installRight.style.borderTopLeftRadius = '25px';
-    installRight.style.paddingLeft = '10px';
-    installLeft.style.backgroundColor = '#E5E2DD';
+    installRight.style.backgroundColor = activeColor;
+    installRight.style.borderTopLeftRadius = '10px';
+    installRight.style.borderTopRightRadius = '0px';
+
+    installLeft.style.backgroundColor = inactiveColor;
+    installLeft.style.borderTopRightRadius = '0px';
+    installLeft.style.borderTopLeftRadius = '10px';
+
     install.style.display = 'none';
-    tg.style.display = 'flex'
+    tg.style.display = 'flex';
 }
 
 btn.onclick = function(){
-    installLeft.style.backgroundColor = 'white';
-    installLeft.style.borderTopRightRadius = '25px';
-    installLeft.style.paddingRight = '10px';
-    installRight.style.backgroundColor = '#E5E2DD';
+    installLeft.style.backgroundColor = activeColor;
+    installLeft.style.borderTopRightRadius = '10px';
+    installLeft.style.borderTopLeftRadius = '0px';
+
+    installRight.style.backgroundColor = inactiveColor;
+    installRight.style.borderTopLeftRadius = '0px';
+    installRight.style.borderTopRightRadius = '10px';
+
     tg.style.display = 'none';
-    install.style.display = 'flex'
+    install.style.display = 'flex';
+    install.style.fontWeight = '800'
 }
